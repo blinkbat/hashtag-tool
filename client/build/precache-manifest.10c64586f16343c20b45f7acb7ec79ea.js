49a264a41d5b2135de854e7cfbeb4d8f421757fe
blinkbat <dbennettmiami@gmail.com>
2020-09-21 14:01:01 -0400

    build

---
+++ client/build/precache-manifest.10c64586f16343c20b45f7acb7ec79ea.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "823b0e140d8997b229f4",
+    "url": "/static/js/main.9e3fe921.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a20ec4e81a94779d376",
+    "url": "/static/js/2.835a8a7f.chunk.js"
+  },
+  {
+    "revision": "e56900102fb238c7931a95df741dda20",
+    "url": "/index.html"
+  }
+];
